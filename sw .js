69fe7298fa9ca7c7b9a3ebb9a0c98a945bc8f343
Mohamed Dah Agove <agwfmhmd@gmail.com>
2025-12-25 21:33:45 +0100

    Add files via upload

---
+++ sw .js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'revision-ba-v67'; // ✅ تحديث مهم: حفظ حالة الترحيب الدائمة
+const assetsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assetsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return fetch(event.request).catch(() => response);
+    })
+  );
+});
